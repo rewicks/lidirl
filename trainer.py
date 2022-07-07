cca572481e5e648a81056b15cdd11bfc6af7e561
Rachel Wicks <rachel@wicks.me>
2022-07-07 10:26:59 -0400

    update from grid

--- trainer.py
+++ trainer.py
@@ -211,6 +211,7 @@ def parse_args():
     parser.add_argument("--max_epochs", type=int, default=100)
     parser.add_argument("--save_every_epoch", action="store_true", default=False)
     parser.add_argument("--hidden_dim", type=int, default=256)
+    parser.add_argument("--validation_threshold", type=int, default=10)
 
     args = parser.parse_args()
     return args
