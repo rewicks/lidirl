warmup rate; masking fixes

lidirl/models.py
@@ -584,7 +584,7 @@ class HierarchicalRoformer(nn.Module):
             inputs = torch.cat((inputs, pads), dim=1)
 
         mask = (inputs!=0).clone().detach()
-        mask = (inputs == inputs)
+        # mask = (inputs == inputs)
 
         # create windows
         inputs = inputs.reshape(inputs.shape[0], -1, self.window_size)
@@ -596,12 +596,12 @@ class HierarchicalRoformer(nn.Module):
         window_mask = mask.reshape(-1, self.window_size).clone().detach()
         window_mask[~torch.any(window_mask, dim=-1)] = True        
         embeds = self.embed_layer(strides) # embed inputs
-        embeds = self.pos_embed(embeds)
+        # embeds = self.pos_embed(embeds)
         window_encoding = torch_max_no_pads(self.window_encoder(inputs_embeds=embeds, encoder_attention_mask=window_mask).last_hidden_state, window_mask, flip=True) # take mean across window sequence
         windows = window_encoding.reshape(batch_size, seq_len, -1) # re-insert the original sequence length dimension
         mask = torch.any(mask, dim=-1)
 
-        windows = self.global_pos_embed(windows)
+        # windows = self.global_pos_embed(windows)
         encoding = torch_max_no_pads(self.global_encoder(inputs_embeds=windows, encoder_attention_mask=mask).last_hidden_state, mask, flip=True) # take mean across global sequence
         output = self.proj(encoding) # project onto labels (raw logits)
         return output

lidirl/trainer.py
@@ -22,6 +22,7 @@ if (__package__ is None or __package__ == "") and __name__ == '__main__':
 from . import __version__
 from .preprocessor import Dataset, Processor, PaddedProcessor, NGramProcessor, VisRepProcessor
 from .models import CLD3Model, TransformerModel, ConvModel, UNETModel, RoformerModel, Hierarchical, HierarchicalRoformer
+from .lr_scheduler import NoamOpt
 
 from .metrics import Results
 from .logger import TrainingLogger
@@ -92,16 +93,13 @@ class Trainer():
             self.criterion = nn.CrossEntropyLoss(label_smoothing=0.1, reduction='sum')
             
         self.lr = args.lr
-        # self.warmup_lr = args.warmup_lr
-        # self.warmup_updates = args.warmup_updates
+        self.warmup_lr = args.warmup_lr
+        self.warmup_updates = args.warmup_updates
 
-        # lr = self.warmup_lr if (args.warmup_lr is not None and args.warmup_updates is not None) else self.lr
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr, betas=(0.9, 0.98), eps=1e-9)
         self.optimizer.zero_grad()
 
-        # if self.args.warmup_lr is not None and self.args.warmup_updates is not None:
-        #     scheduler = LinearLR(self.optimizer, total_iters=args.warmup_updates)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, 500, gamma=0.99)
+        self.scheduler = NoamOpt(self.lr, self.warmup_updates, self.warmup_lr, self.optimizer)
 
         total_params = sum([p.numel() for p in self.model.parameters()])
         logger.info(f'Training with: {total_params} total parameters')