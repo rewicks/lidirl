masking fix for bytes; mamba stub for nick

lidirl/dataloader.py
@@ -91,13 +91,6 @@ class VisRepProcessor():
 ####################################### DATASET ######################################
 
 
-# def file_gen(file_name):
-#     while True:
-#         with open(file_name) as inf:
-#             for index, line in enumerate(inf):
-#                 if len(line.strip().split('\t')) == 2:
-#                     yield line, index
-
 class Dataset(torch.utils.data.IterableDataset):
     def __init__(self,
                     train_files,
@@ -109,7 +102,9 @@ class Dataset(torch.utils.data.IterableDataset):
                     visual_processor = None,
                     num_workers = 1,
                     augmentations = None,
-                    augmentation_prob = 0.0):
+                    augmentation_prob = 0.0,
+                    vocab_length = None,
+                    character_coverage = 1.0):
         super(Dataset).__init__()
 
         logger.info(f"Creating a {type} dataset with {input_type} inputs.")
@@ -127,7 +122,7 @@ class Dataset(torch.utils.data.IterableDataset):
         # both vocab and labels should either be passed or be None
         if vocab is None:
             logger.info("Vocab was not passed. Builing vocabulary and label index.")
-            self.vocab, self.labels = self.build_vocab_and_labels(train_files)
+            self.vocab, self.labels = self.build_vocab_and_labels(train_files, vocab_length=vocab_length, character_coverage = character_coverage)
         else:
             logger.info("Using predefined vocabulary and label index.")
             self.vocab = vocab
@@ -203,6 +198,8 @@ class Dataset(torch.utils.data.IterableDataset):
         vocab = {}
         labels = {}
 
+        vocab["[PAD]"] = 0
+
         vocab_counter = Counter()
         for t in train_files:
             with open(t) as inf:
@@ -211,7 +208,8 @@ class Dataset(torch.utils.data.IterableDataset):
                     if len(line) > 1 and len(line[1].strip()) > 0:
                         labels = add_label(labels, line[0])
                         vocab_counter.update([t for t in line[1]])
-                        if self.augmentations is not None and random.random() < self.augmentation_prob:
+                        # marginally faster for byte representations
+                        if self.input_type != 'bytes' and self.augmentations is not None and random.random() < self.augmentation_prob:
                             aug = self.get_augmentation()
                             if type(aug) is not Codeswitch:
                                 new_text = aug(line[1].strip())
@@ -222,27 +220,26 @@ class Dataset(torch.utils.data.IterableDataset):
         if vocab_length is None:
             vocab_length = character_coverage * len(vocab_counter)
 
+        # for visual representations we will cache the most frequent characters
+        if self.input_type == "visual":
+            logger.info(f"Visual representation inputs. Will precache {int(vocab_length)} most frequent characters.")
+            for v, _ in vocab_counter[:int(vocab_length)]:
+                vocab[v] = self.visual_processor.build_image(v)
+
 
         # We only keep the highest frequency characters in our vocabulary
         # add UNK
-        vocab["[PAD]"] = 0
-        if self.input_type == "characters":
+        elif self.input_type == "characters":
             vocab["[UNK]"] = 1
             logger.info(f"Total vocab size for character model is {int(vocab_length)}")
             for v, _ in vocab_counter[:int(vocab_length)]:
                 vocab[v] = len(vocab.keys())
             
         # Bytes obviously ignores and uses the byte number as the index
-        elif self.input_type == "bytes":
+        else:
             logger.info("Using byte level model. Vocab size 256.")
             for _ in range(256):
                 vocab[str(_)] = _
-
-        # for visual representations we will cache the most frequent characters
-        else:
-            logger.info(f"Visual representation inputs. Will precache {int(vocab_length)} most frequent characters.")
-            for v, _ in vocab_counter[:int(vocab_length)]:
-                vocab[v] = self.visual_processor.build_image(v)
 
         return vocab, labels
 
@@ -429,6 +426,12 @@ class BatchCollator():
 
         return labels
    
+    def create_mask(self, texts):
+        lengths = [len(t) for t in texts]
+        mask = torch.zeros((len(lengths), max(lengths)), dtype=torch.bool)
+        for i, l in enumerate(lengths):
+            mask[i][l:] = True
+        return mask
 
     def __call__(self, samples):
         max_size = 0
@@ -440,10 +443,11 @@ class BatchCollator():
             labels.append(label)
             texts.append(text)
 
+        mask = self.create_mask(texts)
         padded_text = self.pad_texts(texts, max_size=max_size)
         processed_labels = self.process_labels(labels)
 
-        return padded_text, processed_labels
+        return padded_text, processed_labels, mask
 
             
 class ValidationDataLoader():
@@ -479,7 +483,9 @@ def build_datasets(args, augmentations):
                                 visual_processor=visproc,
                                 temperature=args.temperature,
                                 augmentations=augmentations,
-                                augmentation_prob=args.augmentation_probability)
+                                augmentation_prob=args.augmentation_probability,
+                                vocab_length = args.vocab_length,
+                                character_coverage = args.character_coverage)
 
     token_level = True if args.pred_type == "token_level" else False
 

lidirl/models.py
@@ -165,9 +165,10 @@ class RoformerModel(nn.Module):
             self.proj = nn.Linear(hidden_dim, label_size)
 
 
-    def forward(self, inputs):
+    def forward(self, inputs, mask=None):
         inputs = inputs[:, :self.max_position_embeddings]
-        mask = (inputs!=0).clone().detach()
+        # mask = (inputs!=0).clone().detach()
+        mask = ~mask
         model_out = self.model(inputs, attention_mask=mask).last_hidden_state
         if self.pred_type != "token_level":
             model_out = torch_max_no_pads(model_out, mask, flip=True)
@@ -436,20 +437,24 @@ class Hierarchical(nn.Module):
         self.num_layers = num_layers
         self.pred_type = pred_type
 
-    def forward(self, inputs):
+    def forward(self, inputs, mask=None):
 
         # truncate inputs if too long
         inputs = inputs[:, :self.max_length]
+        mask = mask[:, :self.max_length]
 
         if (inputs.shape[1] % self.window_size) != 0:
             # determine how much padding is necessary to make all windows the same length
             pad_size = self.window_size - (inputs.shape[1] % self.window_size)
             pads = torch.zeros((inputs.shape[0], pad_size), device=inputs.device, dtype=torch.long)
+            mask_pads = torch.ones((inputs.shape[0], pad_size), device=inputs.device, dtype=torch.bool)
+
 
             # add padding to the input
             inputs = torch.cat((inputs, pads), dim=1)
+            mask = torch.cat((mask, mask_pads), dim=1)
 
-        mask = (inputs==0).clone().detach()
+        # mask = (inputs==0).clone().detach()
 
         # create windows
         inputs = inputs.reshape(inputs.shape[0], -1, self.window_size)
@@ -570,10 +575,11 @@ class HierarchicalRoformer(nn.Module):
         mask = torch.sum(inputs,dim=-1) == 0
         return mask
 
-    def forward(self, inputs):
+    def forward(self, inputs, mask=None):
 
         # truncate inputs if too long
         inputs = inputs[:, :self.max_length]
+        mask = mask[:, :self.max_length]
 
         if (inputs.shape[1] % self.window_size) != 0:
             # determine how much padding is necessary to make all windows the same length
@@ -583,7 +589,8 @@ class HierarchicalRoformer(nn.Module):
             # add padding to the input
             inputs = torch.cat((inputs, pads), dim=1)
 
-        mask = (inputs!=0).clone().detach()
+        mask = ~mask
+        # mask = (inputs!=0).clone().detach()
         # mask = (inputs == inputs)
 
         # create windows
@@ -698,7 +705,7 @@ class UNETModel(nn.Module):
         self.proj = nn.Linear(self.chs[1], label_size)
 
     
-    def forward(self, inputs):
+    def forward(self, inputs, mask=None):
         inputs = inputs[:, :self.length]
         emb = self.embed(inputs).transpose(1,2)
         encoding = self.encoder(emb)
@@ -1052,3 +1059,65 @@ class FlashModel(nn.Module):
         return save
 
 
+############################## MAMBA #############################
+
+
+
+class Mamba(nn.Module):
+    def __init__(self, 
+                    vocab_size : int,
+                    label_size : int,
+                    embed_dim=256,
+                    hidden_dim=256,
+                    max_length=512, # I'm not sure if you need this?
+                    num_layers=3,
+                    montecarlo_layer = False,
+                    pred_type = "multiclass"
+                    ):
+        super().__init__()
+
+
+        """ I don't know if you need this, but if you just use self.proj in forward for projection then it'll be compatible with the noise-aware training"""
+        # noise-aware labels layer or traditional linear projection
+        if montecarlo_layer:
+            self.proj = MCSoftmaxDenseFA(hidden_dim, label_size, 1, logits_only=True)
+        else:
+            self.proj = nn.Linear(embed_dim, label_size)
+
+        self.vocab_size = vocab_size
+        self.embed_dim = embed_dim
+        self.hidden_dim = hidden_dim
+        self.max_length = max_length
+        self.label_size = label_size
+        self.montecarlo_layer = montecarlo_layer
+        self.num_layers = num_layers
+        self.pred_type = pred_type
+
+    def forward(self, inputs, mask=None):
+
+        # inputs are batch size x sequence length
+        # mask is the same. True where padded.
+
+        # REPLACE THIS:
+        encoding = torch.randn((inputs.shape[0], self.hidden_dim), device=inputs.device)
+
+        # I think this should be the same
+        output = self.proj(encoding) # project onto labels (raw logits)
+        return output # output should be raw logits as softmax or sigmoid is applied separately (dependent on pred_type)
+
+    def save_object(self):
+        """
+        If you add any hyperparameters, could you also save them here.
+        """
+        save = {
+            "weights": self.state_dict(),
+            "vocab_size": self.vocab_size,
+            "embed_dim": self.embed_dim,
+            "hidden_dim": self.hidden_dim,
+            "label_size": self.label_size,
+            "montecarlo_layer": self.montecarlo_layer,
+            "max_length": self.max_length,
+            "num_layers": self.num_layers,
+            "pred_type" : self.pred_type
+        }
+        return save  

lidirl/trainer.py
@@ -21,7 +21,7 @@ if (__package__ is None or __package__ == "") and __name__ == '__main__':
 
 from . import __version__
 from .preprocessor import Dataset, Processor, PaddedProcessor, NGramProcessor, VisRepProcessor
-from .models import CLD3Model, TransformerModel, ConvModel, UNETModel, RoformerModel, Hierarchical, HierarchicalRoformer
+from .models import CLD3Model, TransformerModel, ConvModel, UNETModel, RoformerModel, Hierarchical, HierarchicalRoformer, Mamba
 from .lr_scheduler import NoamOpt
 
 from .metrics import Results
@@ -84,6 +84,7 @@ class Trainer():
         if not os.path.exists(args.output_path):
             os.makedirs(args.output_path, exist_ok=True)
 
+
         self.model = model.to(self.device)
         logger.info(self.model)
 
@@ -118,10 +119,13 @@ class Trainer():
         self.iswarm = False
         logger.info(args)
 
-        wandb_config = {
-            "project_name": args.wandb_proj,
-            "run_name": args.wandb_run
-        }
+        if args.wandb_proj is not None:
+            wandb_config = {
+                "project_name": args.wandb_proj,
+                "run_name": args.wandb_run
+            }
+        else:
+            wandb_config = None
 
         self.train_log = TrainingLogger(stdout=True,
                                             wandb_config=wandb_config)
@@ -129,22 +133,23 @@ class Trainer():
         self.num_updates = 0
 
     def run_epoch(self, args, epoch=0):
-        for batch_index, (texts, targets) in enumerate(self.train_dataset, 1):
+        for batch_index, (texts, targets, masks) in enumerate(self.train_dataset, 1):
             try:
-                # multilabel means that every target class with greater than 0 prob should be correct label
-                if args.pred_type == "multilabel":
-                    for trg in targets:
-                        targets[trg > 0] = 1
-
                 inputs = texts.to(self.device)
                 targets = targets.to(self.device)
+                masks = masks.to(self.device)
 
-                output = self.model(inputs) # raw logits
-                
+                # if torch.any(torch.isinf(self.model.embed_layer.weight)).item() or torch.any(torch.isnan(self.model.embed_layer.weight)).item():
+                #     breakpoint()
+
+                output = self.model(inputs, mask=masks) # raw logits
+            
+                # multilabel means that every target class with greater than 0 prob should be correct label
                 # multiabel--there should be some threshold logic here, but alas
                 # by default, we'll just use sigmoid (0.5 threshold)
                 if args.pred_type == "multilabel":
                     output = F.sigmoid(output)
+                    targets[targets > 0] = 1
                     labels = targets
 
                 # traditional, multiclass classifier
@@ -164,7 +169,10 @@ class Trainer():
                 else:
                     loss = self.criterion(output, labels)
 
-                    ppl = torch.exp(F.cross_entropy(output, targets)/args.update_interval).item()
+                    try:
+                        ppl = torch.exp(F.cross_entropy(output, targets)/args.update_interval).item()
+                    except:
+                        breakpoint()
 
                 # keep track of metrics for eventually logging
                 self.results.calculate(loss.item()/args.update_interval, ppl, output, targets, args.pred_type == "token_level")
@@ -189,6 +197,8 @@ class Trainer():
                     if self.num_updates == args.max_updates:
                         logger.info(f"Reached maximum number of updates ({args.max_updates}) for training. Stopping.")
                         return 0
+                    # if torch.any(torch.isinf(self.model.embed_layer.weight)).item() or torch.any(torch.isnan(self.model.embed_layer.weight)).item():
+                    #     breakpoint()
 
                 if (batch_index) % (args.log_interval * args.update_interval) == 0:
                     batch_results = self.results.get_results(self.optimizer.param_groups[0]['lr'], completed=batch_index+1)
@@ -306,15 +316,17 @@ class Trainer():
                                         device=self.device,
                                         type='VALIDATION')
             with torch.no_grad():
-                for batch_index, (texts, targets) in enumerate(val, 1):
+                for batch_index, (texts, targets, masks) in enumerate(val, 1):
                     inputs = texts.to(self.device)
                     targets = targets.to(self.device)
-                    output = self.model(inputs)
+                    masks = masks.to(self.device)
 
-                    if args.pred_type != "multilabel":
-                        labels = torch.argmax(targets, dim=1)
-                    else:
-                        labels = targets
+                    output = self.model(inputs, mask=masks)
+
+                    # if args.pred_type != "multilabel":
+                    #     labels = torch.argmax(targets, dim=1)
+                    if args.pred_type == "multilabel":
+                        targets[targets > 0] = 1
                         output = F.sigmoid(output)
 
                     if args.model == "unet" or args.pred_type == "token_level":
@@ -430,6 +442,17 @@ def build_model(args, vocab, labels):
                     num_layers=args.num_layers,
                     pred_type=args.pred_type,
         )
+    elif args.model == "mamba":
+        model = Mamba(
+            vocab_size=len(vocab),
+            label_size=len(labels),
+            embed_dim=args.embedding_dim,
+            hidden_dim=args.hidden_dim,
+            max_length=args.max_length,
+            num_layers=args.num_layers,
+            pred_type=args.pred_type,
+            montecarlo_layer=args.montecarlo_layer
+        )
 
     return model
 
@@ -449,6 +472,15 @@ def parse_args():
                                         help="The path to the validation data.")
     parser.add_argument('--smart_group_validation_files', default=False, action="store_true",
                             help="If passed, will group validation sets into separately logged clusters based on each file's parent directory")
+
+    parser.add_argument("--vocab_length",
+                                        type=int,
+                                        default=None,
+                                        help="How large to limit the vocab size")
+    parser.add_argument("--character_coverage",
+                                        type=float,
+                                        default=1.0,
+                                        help="What percent (0,1] of the characters/bytes to cover")
 
     parser.add_argument("--output_path", 
                                         required=True,
@@ -626,6 +658,10 @@ def parse_args():
     hierachical_parser.add_argument("--nhead", default=8, type=int)
     hierachical_parser.add_argument("--window-size", default=8, type=int)
     hierachical_parser.add_argument("--stride", default=1, type=int)
+
+
+    mamba_parser = subparsers.add_parser("mamba", help="a mamba model")
+    mamba_parser.add_argument("--max-length", default=1024, type=int)
 
     args = parser.parse_args()
 